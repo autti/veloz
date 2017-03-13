38c8ac47537b6f891fa59d7c6e67bc641e3a0e7a
Ariel Nunez <ingenieroariel.com>
2017-03-13 11:30:25 -0500

    add other dump files

--- canable_setup.sh
+++ canable_setup.sh
@@ -1,2 +1,4 @@
+sudo slcand -o -c -s6 /dev/serial/by-id/usb-CANtact_CANtact_dev_00000000001A-if00 can0
+sudo ifconfig can0 up
 sudo slcand -o -c -s6 /dev/serial/by-id/usb-CANtact_CANtact_dev_00000000001A-if00 can1
 sudo ifconfig can1 up
